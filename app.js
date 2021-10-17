f01c286b30436b5690754a96861e36ef7d6009c7
Tirtharaj <tirtharajjana615@gmail.com>
2021-10-17 22:21:16 +0530

    Post schema and Create post route

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const mongoose = require('mongoose');
 const { MONGOURI } = require('./keys');
 
 require('./models/user');
+require('./models/post');
 
 app.use(express.json());
 app.use(require('./routes/auth.js'));
+app.use(require('./routes/post.js'));
 
 mongoose.connect(MONGOURI, {
     useNewUrlParser: true,
--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -17,8 +17,8 @@ module.exports = (req, res, next) => {
         const { _id } = payload;
         User.findById(_id).then(userData => {
             req.user = userData;
+            next();
         })
-        next();
     })
 
 }
