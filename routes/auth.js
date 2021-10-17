5306046881b91f254751b41c7c1c52fd051b2401
Tirtharaj <tirtharajjana615@gmail.com>
2021-10-17 10:48:58 +0530

    Creating Sign in/Login route

--- routes/auth.js
+++ routes/auth.js
@@ -38,4 +38,29 @@ router.post('/signup', (req, res) => {
 
 })
 
+router.post('/signin', (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(422).json({ error: "Please add email or password" })
+    }
+    User.findOne({ email })
+        .then(savedUser => {
+            if (!savedUser) {
+                return res.status(422).json({ error: "Invalid Email or password" })
+            }
+            bcrypt.compare(password, savedUser.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        return res.json({ message: "Successfully Signed in" })
+                    }
+                    else {
+                        return res.status(422).json({ error: "Invalid Email or password" })
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        })
+})
+
 module.exports = router;
