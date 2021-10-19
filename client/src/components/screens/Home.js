8a5d0b9324393796e08cff37f2a6f26708e55de9
Tirtharaj <tirtharajjana615@gmail.com>
2021-10-19 21:44:42 +0530

    Creating Home page UI

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,8 +2,55 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className="home" >
+
+            <div className="card home-card" >
+                <h5>Tirtharaj</h5>
+                <div className="card-image" >
+                    <img src="https://media.istockphoto.com/photos/abstract-blurred-office-interior-background-picture-id1313115119?b=1&k=20&m=1313115119&s=170667a&w=0&h=E4WPdhmRrjCcQHXszYhhfg1nOMHD7vGtpL4usT9KOwM=" alt="pic" />
+                </div>
+                <div className="card-content" >
+                    <i class="material-icons" style={{ color: "red" }} >favorite</i>
+                    <h6>title</h6>
+                    <p>this is amazing</p>
+                    <input type='text' placeholder="Add comment" />
+                </div>
+            </div>
+            <div className="card home-card" >
+                <h5>Tirtharaj</h5>
+                <div className="card-image" >
+                    <img src="https://media.istockphoto.com/photos/abstract-blurred-office-interior-background-picture-id1313115119?b=1&k=20&m=1313115119&s=170667a&w=0&h=E4WPdhmRrjCcQHXszYhhfg1nOMHD7vGtpL4usT9KOwM=" alt="pic" />
+                </div>
+                <div className="card-content" >
+                    <h6>title</h6>
+                    <p>this is amazing</p>
+                    <input type='text' placeholder="Add comment" />
+                </div>
+            </div>
+            <div className="card home-card" >
+                <h5>Tirtharaj</h5>
+                <div className="card-image" >
+                    <img src="https://media.istockphoto.com/photos/abstract-blurred-office-interior-background-picture-id1313115119?b=1&k=20&m=1313115119&s=170667a&w=0&h=E4WPdhmRrjCcQHXszYhhfg1nOMHD7vGtpL4usT9KOwM=" alt="pic" />
+                </div>
+                <div className="card-content" >
+                    <h6>title</h6>
+                    <p>this is amazing</p>
+                    <input type='text' placeholder="Add comment" />
+                </div>
+            </div>
+            <div className="card home-card" >
+                <h5>Tirtharaj</h5>
+                <div className="card-image" >
+                    <img src="https://media.istockphoto.com/photos/abstract-blurred-office-interior-background-picture-id1313115119?b=1&k=20&m=1313115119&s=170667a&w=0&h=E4WPdhmRrjCcQHXszYhhfg1nOMHD7vGtpL4usT9KOwM=" alt="pic" />
+                </div>
+                <div className="card-content" >
+                    <h6>title</h6>
+                    <p>this is amazing</p>
+                    <input type='text' placeholder="Add comment" />
+                </div>
+            </div>
+
+
         </div>
     )
 }
