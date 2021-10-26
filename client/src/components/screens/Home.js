41c4e6be6efd4ffa791f65ff92aaa3cab2fe6338
Tirtharaj Jana <55793707+tirtharajjana@users.noreply.github.com>
2021-10-26 15:57:24 +0530

    like unlike Implementation in react js

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react';
+import { UserContext } from '../../App';
 
 const Home = () => {
     const [data, setData] = useState([]);
+    const { state, dispatch } = useContext(UserContext);
+    // console.log(state);
     useEffect(() => {
         fetch('/allpost', {
             headers: {
@@ -9,10 +12,57 @@ const Home = () => {
             }
         }).then(res => res.json())
             .then(result => {
-                // console.log(result.posts);
+                console.log(result.posts);
                 setData(result.posts);
             })
     }, [])
+
+    const likePost = (id) => {
+        fetch('/like', {
+            method: 'put',
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem('jwt'),
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                postId: id
+            })
+        }).then(res => res.json())
+            .then(result => {
+                const newData = data.map(item => {
+                    if (item._id === result._id) {
+                        return result;
+                    } else {
+                        return item;
+                    }
+                })
+                setData(newData);
+            }).catch(err => console.log(err))
+    }
+
+    const unlikePost = (id) => {
+        fetch('/unlike', {
+            method: 'put',
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem('jwt'),
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                postId: id
+            })
+        }).then(res => res.json())
+            .then(result => {
+                const newData = data.map(item => {
+                    if (item._id === result._id) {
+                        return result;
+                    } else {
+                        return item;
+                    }
+                })
+                setData(newData);
+            }).catch(err => console.log(err))
+    }
+
     return (
         <div className="home" >
             {
@@ -24,7 +74,12 @@ const Home = () => {
                                 <img src={item.photo} alt="pic" />
                             </div>
                             <div className="card-content" >
-                                <i className="material-icons" style={{ color: "red" }} >favorite</i>
+                                <i className="material-icons">favorite</i>
+                                {item.likes.includes(state._id)
+                                    ? <i className="material-icons" style={{ color: "red" }} onClick={() => unlikePost(item._id)}>thumb_down</i>
+                                    : <i className="material-icons" style={{ color: "red" }} onClick={() => likePost(item._id)} >thumb_up</i>
+                                }
+                                <h6>{item.likes.length} likes</h6>
                                 <h6>{item.title}</h6>
                                 <p>{item.body}</p>
                                 <input type='text' placeholder="Add comment" />
--- models/post.js
+++ models/post.js
@@ -5,7 +5,7 @@ const postSchema = new mongoose.Schema({
     title: { type: String, required: true },
     body: { type: String, required: true },
     photo: { type: String, required: true },
-    likes: { type: ObjectId, ref: "User" },
+    likes: { type: [ObjectId], ref: "User", default: [] },
     postedBy: { type: ObjectId, ref: "User", required: true },
 })
 
