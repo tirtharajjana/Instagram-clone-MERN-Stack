5898029296ebe25f8109348d57d600de449d3ba5
Tirtharaj Jana <55793707+tirtharajjana@users.noreply.github.com>
2021-10-26 14:55:53 +0530

    Like and unlike post node js API

--- routes/post.js
+++ routes/post.js
@@ -34,4 +34,30 @@ router.get('/mypost', requireLogin, (req, res) => {
         .catch(err => console.log(err))
 })
 
+router.put('/like', requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId, {
+        $push: { likes: req.user._id }
+    }, { new: true })
+        .exec((err, result) => {
+            if (err) {
+                return res.status(422).json({ error: error })
+            } else {
+                return res.json(result)
+            }
+        })
+})
+
+router.put('/unlike', requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId, {
+        $pull: { likes: req.user._id }
+    }, { new: true })
+        .exec((err, result) => {
+            if (err) {
+                return res.status(422).json({ error: error })
+            } else {
+                return res.json(result)
+            }
+        })
+})
+
 module.exports = router;
