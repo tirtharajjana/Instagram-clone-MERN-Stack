587bddaf2ccefa2e8487734c422515547b59e7d8
Tirtharaj Jana <55793707+tirtharajjana@users.noreply.github.com>
2021-10-28 22:02:13 +0530

    Saving updated Profile pic in database

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -4,7 +4,11 @@ import { UserContext } from '../../App'
 const Profile = () => {
     const [myPics, setMyPics] = useState([]);
     const { state, dispatch } = useContext(UserContext);
-    console.log(state);
+    const [image, setImage] = useState('')
+    const [url, setUrl] = useState('')
+
+    // console.log(state);
+
     useEffect(() => {
         fetch('/mypost', {
             headers: {
@@ -15,15 +19,75 @@ const Profile = () => {
                 setMyPics(result.myPost)
             })
     }, [])
+
+    useEffect(() => {
+        if (image) {
+            const data = new FormData();
+            data.append('file', image);
+            data.append('upload_preset', 'insta-clone');
+            data.append('cloud_name', 'tirtharaj');
+
+            fetch('https://api.cloudinary.com/v1_1/tirtharaj/image/upload', {
+                method: 'post',
+                body: data
+            })
+                .then(res => res.json())
+                .then(data => {
+                    // console.log(data);
+
+                    setUrl(data.url);
+                    localStorage.setItem('user', JSON.stringify({ ...state, pic: data.url }));
+                    dispatch({ type: "UPDATEPIC", payload: data.url });
+
+                    fetch('/updatepic', {
+                        method: "put",
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Authorization": "Bearer " + localStorage.getItem("jwt")
+                        },
+                        body: JSON.stringify({
+                            pic: data.url
+                        })
+                    }).then(res => res.json())
+                        .then(result => {
+                            console.log(result)
+                            localStorage.setItem("user", JSON.stringify({ ...state, pic: result.pic }))
+                            dispatch({ type: "UPDATEPIC", payload: result.pic })
+                            //window.location.reload()
+                        })
+
+
+                })
+                .catch(err => console.log(err))
+
+        }
+    }, [image])
+
+    const updatePhoto = (file) => {
+        setImage(file)
+
+    }
+
+
     return (
         <div style={{ maxWidth: '550px', margin: '0 auto' }} >
-            <div style={{ display: "flex", justifyContent: "space-around", margin: "18px 0px", borderBottom: "1px solid grey" }}>
-                <div >
-                    <img style={{ width: "160px", height: "160px", borderRadius: "50%" }} alt='logo'
-                        src={state ? state.pic : "https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif    "}
-                    />
-                </div>
-                <div>
+            <div style={{
+                margin: "18px 0px",
+                borderBottom: "1px solid grey"
+            }}>
+
+
+                <div style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+
+                }}>
+                    <div>
+                        <img style={{ width: "160px", height: "160px", borderRadius: "80px" }}
+                            src={state ? state.pic : "loading"} alt="logo"
+                        />
+
+                    </div>
                     <div>
                         <h4>{state ? state.name : "loading"}</h4>
                         <h5>{state ? state.email : "loading"}</h5>
@@ -33,6 +97,16 @@ const Profile = () => {
                             <h6>{state ? state.following.length : "0"} following</h6>
                         </div>
 
+                    </div>
+                </div>
+
+                <div className="file-field input-field" style={{ margin: "10px" }}>
+                    <div className="btn #64b5f6 blue darken-1">
+                        <span>Update pic</span>
+                        <input type="file" onChange={(e) => updatePhoto(e.target.files[0])} />
+                    </div>
+                    <div className="file-path-wrapper">
+                        <input className="file-path validate" type="text" />
                     </div>
                 </div>
             </div>
