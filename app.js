17aed8f088faeb3321e8303548904d875420392a
Tirtharaj Jana <55793707+tirtharajjana@users.noreply.github.com>
2021-10-29 09:32:52 +0530

    dev

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const { MONGOURI } = require('./keys');
+const { MONGOURI } = require('./config/keys');
 
 require('./models/user');
 require('./models/post');
@@ -23,11 +23,19 @@ mongoose.connection.on('error', (err) => {
 })
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 app.get('/', (req, res) => {
     res.send("hi");
 })
+
+if (process.env.NODE_ENV == 'production') {
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.listen(PORT, () => console.log('Server is running on', PORT));
