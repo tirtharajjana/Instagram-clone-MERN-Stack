49d27b323ed4c653aa09be8f3359a5cf777e37d6
Tirtharaj Jana <55793707+tirtharajjana@users.noreply.github.com>
2021-10-29 09:38:45 +0530

    dev

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.get('/', (req, res) => {
     res.send("hi");
 })
 
-if (process.env.NODE_ENV == 'production') {
-    app.use(express.static('client/build'));
-    const path = require('path');
-    app.get('*', (req, res) => {
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
